400c6df13dd2e0adf7df73996d2224cd69a1817e
Szilárd Kósa <kszilard777@gmail.com>
2021-03-24 20:13:01 +0100

    splitter added

--- src/features/simulator/SimulatorPage.tsx
+++ src/features/simulator/SimulatorPage.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import Navbar from './Navbar'
+import SplitterLayout from 'react-splitter-layout'
+import './Splitter.css'
+import { Box } from '@chakra-ui/layout'
 
 const SimulatorPage = () => {
   let { id } = useParams<{ id: string }>()
@@ -9,7 +12,16 @@ const SimulatorPage = () => {
   return (
     <>
       <Navbar />
-      <div>Sim page</div>
+      <Box h="calc(100vh - 64px)" w="full" position="absolute">
+        <SplitterLayout>
+          <Box bg="lightseagreen" h="100%">
+            Pane 1
+          </Box>
+          <Box bg="lightskyblue" h="100%">
+            Pane 2
+          </Box>
+        </SplitterLayout>
+      </Box>
     </>
   )
 }
