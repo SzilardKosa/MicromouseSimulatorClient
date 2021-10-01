7f030aba69402eef58de3cddba1c676a9b4fb1cc
Szilárd Kósa <kosa.szilard@mirango.hu>
2021-10-01 19:25:48 +0200

    Nicer loading and error state added

--- src/features/workspace/simulation-list-page/SimulationListPage.tsx
+++ src/features/workspace/simulation-list-page/SimulationListPage.tsx
@@ -1,22 +1,35 @@
 import React from 'react'
-import { Box } from '@chakra-ui/react'
+import { Box, Center, Spinner, Text, useColorModeValue } from '@chakra-ui/react'
 import { useSimulations } from '../../../api/hooks/simulations'
 import SimulationList from './SimulationList'
 
 const SimulationListPage = () => {
   const { status, data, error } = useSimulations()
+  const errorColor = useColorModeValue('red.700', 'red.300')
+  let content
 
   if (status === 'loading') {
-    return <div>Loading...</div>
-  }
-
-  if (status === 'error') {
-    return <div>Error: {error?.message}</div>
+    content = (
+      <Center h="full">
+        <Spinner />
+      </Center>
+    )
+  } else if (status === 'error') {
+    console.log(error)
+    content = (
+      <Center h="full">
+        <Text fontSize="md" color={errorColor}>
+          An error occured while loading the simulations!
+        </Text>
+      </Center>
+    )
+  } else {
+    content = <SimulationList simulations={data ?? []} />
   }
 
   return (
     <Box h="calc(100vh - 64px)" w="full" position="relative">
-      <SimulationList simulations={data ?? []} />
+      {content}
     </Box>
   )
 }
