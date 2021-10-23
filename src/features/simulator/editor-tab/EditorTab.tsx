4813a01a12e5b9b26369db6f5e8156302ea225df
Szilárd Kósa <kosa.szilard@mirango.hu>
2021-10-23 20:24:37 +0200

    Dark/Light support added to the splitter

--- src/features/simulator/editor-tab/EditorTab.tsx
+++ src/features/simulator/editor-tab/EditorTab.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useColorModeValue } from '@chakra-ui/react'
 import SplitterLayout from 'react-splitter-layout'
 import { SimulationExpandedDTO } from '../../../api/gen'
 import CodeEditorPanel from './code-editor-panel/CodeEditorPanel'
@@ -8,7 +9,11 @@ type EditorTabProps = { simulation: SimulationExpandedDTO }
 
 const EditorTab = ({ simulation }: EditorTabProps) => {
   return (
-    <SplitterLayout percentage secondaryInitialSize={50}>
+    <SplitterLayout
+      percentage
+      secondaryInitialSize={50}
+      customClassName={useColorModeValue('light', 'dark')}
+    >
       <CodeEditorPanel simulation={simulation} />
       <MazeEditorPanel simulation={simulation} />
     </SplitterLayout>
--- src/features/simulator/result-tab/ResultTab.tsx
+++ src/features/simulator/result-tab/ResultTab.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useColorModeValue } from '@chakra-ui/color-mode'
 import SplitterLayout from 'react-splitter-layout'
 import { SimulationExpandedDTO } from '../../../api/gen'
 import ConsolePanel from './console-panel/ConsolePanel'
@@ -10,12 +11,26 @@ type ResultTabProps = { simulation: SimulationExpandedDTO }
 
 const ResultTab = ({ simulation }: ResultTabProps) => {
   return (
-    <SplitterLayout percentage secondaryInitialSize={40}>
-      <SplitterLayout percentage vertical secondaryInitialSize={30}>
+    <SplitterLayout
+      percentage
+      secondaryInitialSize={40}
+      customClassName={useColorModeValue('light', 'dark')}
+    >
+      <SplitterLayout
+        percentage
+        vertical
+        secondaryInitialSize={30}
+        customClassName={useColorModeValue('light', 'dark')}
+      >
         <MazeViewerPanel />
         <ConsolePanel />
       </SplitterLayout>
-      <SplitterLayout percentage vertical secondaryInitialSize={40}>
+      <SplitterLayout
+        percentage
+        vertical
+        secondaryInitialSize={40}
+        customClassName={useColorModeValue('light', 'dark')}
+      >
         <StatsPanel />
         <MouseEditorPanel />
       </SplitterLayout>
