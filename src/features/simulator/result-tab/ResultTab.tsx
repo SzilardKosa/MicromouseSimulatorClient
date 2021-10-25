bc91dcef58251fc2c2819def96cada889e9229a0
Szilárd Kósa <kosa.szilard@mirango.hu>
2021-10-26 00:29:27 +0200

    Added render conditions to ResultTab

--- src/features/simulator/result-tab/ResultTab.tsx
+++ src/features/simulator/result-tab/ResultTab.tsx
@@ -6,6 +6,8 @@ import ConsolePanel from './console-panel/ConsolePanel'
 import MazeViewerPanel from './maze-viewer-panel/MazeViewerPanel'
 import MouseEditorPanel from './mouse-editor-panel/MouseEditorPanel'
 import StatsPanel from './stats-panel/StatsPanel'
+import { Center, Spinner, Text, VStack } from '@chakra-ui/react'
+import ErrorMessageView from '../../../common/ErrorMessageView'
 
 type ResultTabProps = {
   simulation: SimulationExpandedDTO
@@ -13,27 +15,39 @@ type ResultTabProps = {
 }
 
 const ResultTab = ({ simulation, status }: ResultTabProps) => {
+  const colorMode = useColorModeValue('light', 'dark')
+  const maxHeight = 'calc(100vh - 64px)'
+
+  if (status === 'loading') {
+    return (
+      <Center h={maxHeight}>
+        <VStack spacing={2}>
+          <Text fontSize="md">Please wait the simulation is running on the server!</Text>
+          <Spinner />
+        </VStack>
+      </Center>
+    )
+  }
+
+  if (status === 'error') {
+    return (
+      <Center h={maxHeight} p={2}>
+        <ErrorMessageView
+          message={
+            'An error occured while loading the result! Make sure you have strong internet connection!'
+          }
+        />
+      </Center>
+    )
+  }
+
   return (
-    <SplitterLayout
-      percentage
-      secondaryInitialSize={40}
-      customClassName={useColorModeValue('light', 'dark')}
-    >
-      <SplitterLayout
-        percentage
-        vertical
-        secondaryInitialSize={30}
-        customClassName={useColorModeValue('light', 'dark')}
-      >
+    <SplitterLayout percentage secondaryInitialSize={40} customClassName={colorMode}>
+      <SplitterLayout percentage vertical secondaryInitialSize={30} customClassName={colorMode}>
         <MazeViewerPanel />
         <ConsolePanel />
       </SplitterLayout>
-      <SplitterLayout
-        percentage
-        vertical
-        secondaryInitialSize={40}
-        customClassName={useColorModeValue('light', 'dark')}
-      >
+      <SplitterLayout percentage vertical secondaryInitialSize={40} customClassName={colorMode}>
         <StatsPanel />
         <MouseEditorPanel />
       </SplitterLayout>
