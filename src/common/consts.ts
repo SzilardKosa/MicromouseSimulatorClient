36a745387fe0a266bda948f16fe500f026b98ce0
Szilárd Kósa <kosa.szilard@mirango.hu>
2021-11-14 17:33:46 +0100

    Unnecessary escape character removed

--- src/common/consts.ts
+++ src/common/consts.ts
@@ -27,7 +27,7 @@ export function validatePassword(value: string) {
 
 // https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
 export const EMAIL_REGEX =
-  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
 export function validateEmail(value: any) {
   if (!value) {
